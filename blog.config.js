57d6591df937240a3bf0095025e2865f329cd268
Clare123321 <153908043+Clare123321@users.noreply.github.com>
2024-08-02 20:58:19 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Clare', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '为大家分享AI教学，欢迎关注我的B站：b站Clare。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://private.clare11.us.kg/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
